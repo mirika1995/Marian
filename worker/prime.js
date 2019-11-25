147be1c27ef5e73a3876194007a5096b40ce1d9b
mirika1995 <57986253+mirika1995@users.noreply.github.com>
2019-11-25 13:46:05 +0200

    Add files via upload

--- worker/prime.js
+++ worker/prime.js
@@ -1,3 +1,17 @@
+var ruleaza = false;
+
+onmessage = function(e){
+	if (e.data == "start"){
+		ruleaza = true; 
+		
+	}
+	else 
+		if (e.data == "stop"){
+			ruleaza = false;
+			
+		}
+	
+}
 
 function este_prim(n)
 {
@@ -7,7 +21,20 @@ function este_prim(n)
 		return true;
 }
 
-
-for (var i = 1E9; i < 1E11; i++)
+function start(start_value)
+{
+if (ruleaza == false)
+	;
+else {
+for (var i = start_value; i < start_value + 100; i++)
 	   if (este_prim(i))
 		   postMessage(i);
+	   start_value += 100;
+    }
+	if (start_value <1E11)
+setTimeout(start, 1, start_value);
+else
+	postMessage("gata");
+}
+
+start (1E10);
